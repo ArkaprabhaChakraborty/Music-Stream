f6abbd2e2083e24ccd4b3b40bda02e348da80e0e
arkaprabha <chakrabortyarkaprabha998@gmail.com>
2020-10-09 14:20:20 +0530

    Initial

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const Main = ()=>{
+    return(
+        <div className="main">
+            <div className="uppernav">
+                dummy text
+            </div>
+            <div className="mainContext">
+            </div>
+        </div>
+    )
+}
+export default Main
