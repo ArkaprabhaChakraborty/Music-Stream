2ad2812a90ebb0d4a267b9fcd2e5f012dc5f0dd6
arkaprabha <chakrabortyarkaprabha998@gmail.com>
2020-10-12 14:38:49 +0530

    Final Build 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Nav from './components/Nav'
 import Main from './components/Main'
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import { Slider } from '@material-ui/core';
 import './App.scss';
+import { LoopOutlined, VolumeDown, VolumeUp } from '@material-ui/icons';
 
 const App = () => {
   return (
@@ -10,7 +16,31 @@ const App = () => {
         <Nav/>
         <Main/>
       </div>
-    <div className="musicControls">MUSIC</div>
+    <div className="musicControls">
+      <div className="Album">
+        <div className="AlbumImage">
+          <img src="https://d.newsweek.com/en/full/772816/1-5-jupiter-nasa.jpg" alt=""/>
+        </div>
+        <div className="AlbumDetails">
+          <h4>Album name</h4>
+          <p>Singer Name</p>
+        </div>
+      <div className="ControlsLayer">
+          <ShuffleIcon/>
+          <SkipPreviousIcon/>
+          <PlayCircleOutlineIcon className="Play"/>
+          <SkipNextIcon/>
+          <LoopOutlined/>
+      </div>
+      <div className="VolumeSection"> 
+        <VolumeDown/>
+        <br></br>
+        <Slider/>
+        <br></br>
+        <VolumeUp/>
+      </div>
+      </div>
+    </div>
     </div>
   );
 }
--- src/components/pages/Playlists.js
+++ src/components/pages/Playlists.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useParams } from 'react-router';
 import {ReactComponent as PlayIcon} from '../pages/svgs/play.svg'; 
-
+import {ReactComponent as HeartIcon} from '../pages/svgs/heart.svg';
+import {ReactComponent as NoteIcon} from '../pages/svgs/note.svg';
 
 const Play = () =>{
     let {id} = useParams();
@@ -13,25 +14,68 @@ const Play = () =>{
                     <img src="https://d.newsweek.com/en/full/772816/1-5-jupiter-nasa.jpg" alt=""/>
                     </div>
                     <div className="playlistPageContent">
+                    <h5>PLAYLIST</h5>
                     <h1>Title {id}</h1>
-                    <h5>Spotify</h5>
                     <p>Playlist description</p>
+                    <h5>Spotify</h5>
                     </div>
             </div>
+            <br></br>
             <div className="icons">
                 <div className="playbutton">
                     <PlayIcon/>
                 </div>
-                <div className="iconsHeart"></div>
+                <div className="hearticon">
+                    <HeartIcon/>
+                </div>
                 <div className="iconsDots"></div>
             </div>
             <div className="playlistPageSongs">
-                <ul>
-                    <li>Song 1</li>
-                    <li>Song 2</li>
-                    <li>Song 3</li>
-                    <li>Song 4</li>
-                    <li>Song 5</li>
+                <ul className="songlist">
+                    <li>
+                        <div className="songIcon">
+                            <NoteIcon />
+                        </div>
+                        <div className="songDetails">
+                            <h3>Held Down</h3>
+                            <span>Martina Hingis</span>
+                        </div>
+                        <div className="songTime">
+                            <span>4:05</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="songIcon">
+                            <NoteIcon/>
+                        </div>
+                        <div className="songDetails">
+                            <h3>Held Down</h3>
+                            <span>Martina Hingis</span>
+                        </div>
+                        <div className="songTime">
+                            <span>4:05</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="songIcon"><NoteIcon/></div>
+                        <div className="songDetails">
+                            <h3>Held Down</h3>
+                            <span>Martina Hingis</span>
+                        </div>
+                        <div className="songTime">
+                            <span>4:05</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="songIcon"><NoteIcon/></div>
+                        <div className="songDetails">
+                            <h3>Held Down</h3>
+                            <span>Martina Hingis</span>
+                        </div>
+                        <div className="songTime">
+                            <span>4:05</span>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </div>
